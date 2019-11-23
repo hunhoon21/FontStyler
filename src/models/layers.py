118e2d3b0703376c142689cb668fd25cf23a85dc
hunhoon21 <hunhoon21@gmail.com>
2019-11-23 16:59:07 +0900

    reconstruct baseline frame for category vector

---
+++ src/models/layers.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+'''
+TODO : 
+1. How to deal with difference size of kinds of input
+2. Make a FC module
+3. How to apply to CNN Layers
+4. Search Another models
+5. Consider Our structures
+'''
+class Encoder_base(nn.Module):
+    def __init__(self, 
+                 input_category_size=5, input_alpha_size=52, input_font_size=256*256,
+                 z_size=64):
+        super(Encoder_base, self).__init__()
+        input_size = input_category_size + input_alpha_size + input_font_size
+        
+        self.efc1 = nn.Linear(input_size, 8192)
+        self.efc2 = nn.Linear(8192, 4096)
+        self.efc3 = nn.Linear(4096, 2048)
+        self.efc4 = nn.Linear(2048, 1024)
+        self.efc5 = nn.Linear(1024, 256)
+        self.efc6 = nn.Linear(256, z_size)
+    
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.efc1(x))
+        x = F.relu(self.efc2(x))
+        x = F.relu(self.efc3(x))
+        x = F.relu(self.efc4(x))
+        x = F.relu(self.efc5(x))
+        z = self.efc(6)
+        return z
+    
+class Decoder_base(nn.Module):
+    def __init__(self, z_latent_size, z_category_size, z_alpha_size, output_font_size=256*256):
+        super(Decoder_base, self).__init__()
+        
+        z_size = z_latent_size + z_category_size + z_alpha_size
+        self.dfc1 = nn.Linear(z_size, 256)
+        self.dfc2 = nn.Linear(256, 1024)
+        self.dfc3 = nn.Linear(1024, 2048)
+        self.dfc4 = nn.Linear(2048, 4096)
+        self.dfc5 = nn.Linear(4096, 8192)
+        self.dfc6 = nn.Linear(8192, output_font_size)
+        
+    def forward(self, z):
+        
+        z = F.relu(self.dfc1(z))
+        z = F.relu(self.dfc2(z))
+        z = F.relu(self.dfc3(z))
+        z = F.relu(self.dfc4(z))
+        z = F.relu(self.dfc5(z))
+        x_hat = self.dfc6(z)
+        return x_hat
---
+++ src/models/model.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from .layers import Encoder_base, Decoder_base
+
+
+class AE_base(nn.Module):
+    def __init__(self, 
+                 category_size=5, 
+                 alpha_size=52, 
+                 font_size=256*256,
+                 z_size=64,
+                 ):
+        super(AE_base, self).__init__()
+        self.Encoder = Encoder_base(input_category_size=category_size,
+                                    input_alpha_size=alpha_size,
+                                    input_font_size=font_size,
+                                    z_size=z_size)
+        self.Decoder = Decoder_base(z_latent_size=z_size,
+                                    z_category_size=category_size,
+                                    z_alpha_size=alpha_size,
+                                    output_font_size=font_size)
+    
+    def forward(self, x_font, alpha_vector, category_vector=None):
+        x_font = x_font.view(x_font.shape[0], -1)
+        alpha_vector = alpha_vector.view(alpha_vector.shape[0], -1)
+        if category_vector is not None:
+            category_vector = category_vector.view(category_vector.shape[0], -1)
+        
+        if category_vector is not None:
+            x = torch.cat([x_font, category_vector, alpha_vector], dim=1)
+        else:
+            x = torch.cat([x_font, alpha_vector])
+        
+        z_latent = self.Encoder(x)
+        
+        z_latent = z_latent.view(z_latent.shape[0], -1)
+        if category_vector is not None:    
+            z = torch.cat([z_latent, category_vector, alpha_vector], dim=1)
+        else:
+            z = torch.cat([z_latent, alpha_vector], dim=1)
+        
+        x_hat = self.Decoder(z)
+        
+        return x_hat, z_latent
